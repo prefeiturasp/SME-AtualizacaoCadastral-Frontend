9f3a1682361627bff8abd0003acb3ad4c010b163
Luis <lhzimmermann@gmailc.om>
2021-08-11 15:27:57 -0300

    Versão inicial da página de consulta de cadastro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,36 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useRef, useState} from "react";
 import "./home.scss"
 import {Login} from '../../components/Formularios/Login'
+import InputMask from "react-input-mask";
+import {useForm} from "react-hook-form";
+import {YupSignupSchemaConsulta} from "../../utils/ValidacoesAdicionaisFormularios";
+import {BtnCustomizado} from "../../components/BtnCustomizado";
 
 export const Home = () => {
+
+    const {register, handleSubmit, errors} = useForm({
+      mode: "onBlur",
+      //validationSchema: YupSignupSchemaConsulta,
+    });
+
+    const cpfRef = useRef();
+
+    const [showStatus1, setShowStatus1] = useState(false);
+    const [showStatus2, setShowStatus2] = useState(false);
+    const [showStatus3, setShowStatus3] = useState(false);
+    const [showStatusNaoEncontrado, setShowStatusNaoEncontrado] = useState(false);
+
+    const onSubmitCPF = (value, formevent) => {
+      console.log(value)
+      console.log(formevent)
+      setShowStatus1(true)
+      //setShowStatus2(true)
+      //setShowStatus3(true)
+      //setShowStatusNaoEncontrado(true)
+    };
+
+
+
     return (
         <Fragment>
             <div className="w-100 banner-home position-relative">
@@ -10,20 +38,94 @@ export const Home = () => {
                     <div className="conteudo">
                         <div className="col-lg-7 col-sm-12 col-xl-5">
                             <h1 id="conteudo">
-                                Atualize seus dados cadastrais.
+                              Consulte seus dados cadastrais
                             </h1>
                         </div>
                         <div className="col-lg-6 col-sm-12 col-xl-5">
                             <p>
-                            Digite o Código EOL da(o) estudante e a data de nascimento dela(e), para abrir o formulário e completar os dados de cadastro da(o) responsável.
+                              Digite o CPF do responsável pelos(as) estudante(s) cadastro(s) na Rede Municipal de Ensino, e verifique a situação do seu cadastro para realizar as medias necessárias.
                             </p>
                         </div>
+                        <div className="col-lg-6 col-sm-12 col-xl-5">
+                            <span>
+                            Caso não saiba quem está cadastrado(a) ou queria atualizar essa informação, procure a secretaria da escola em que seu(sua) filho(a) está matriculado(a). Os telefones de todas as unidades podem ser consultados no link:
+                            <a href="https://escolaaberta.sme.prefeitura.sp.gov.br/" target="blank">https://escolaaberta.sme.prefeitura.sp.gov.br/</a>
+                            </span>
+                        </div>
+                        
+                        <div className="col-lg-6 col-sm-12 col-xl-5">
+                          <form
+                            onSubmit={handleSubmit(onSubmitCPF)}
+                            name="consultaCadastro"
+                            id="consultaCadastro"
+                          >
+                            <label id="cpf">Informe seu CPF</label>
+                            <div className="row">
+                              <div className="col-lg-8" >
 
+                                <InputMask
+                                    mask="999.999.999-99"
+                                    ref={e => {
+                                      register(e);
+                                      cpfRef.current = e;
+                                    }}
+                                    name="cpf"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="000.000.000-00"
+                                />
+                              {errors.cpf && <span className="span_erro text-white mt-1">{errors.cpf.message}</span>}
+                              </div>
+
+                              <div className="col-lg-4" >
+                                <BtnCustomizado
+                                    type="submit"
+                                    classeCss="btn btn-outline-primary btn-block btn-abrir-formulario"
+                                    texto="Consultar"
+                                />
+                              </div>
+                            </div>
+                          </form>
+                        </div>
+                        
+
+                        <div className="col-lg-6 col-sm-12 col-xl-5 mt-3">
+
+                          {showStatus1 && 
+                            <div className="container-resultado cont-amarelo"> 
+                              Dirija-se a DRE pertencente a sua unidade escolar com os documentos necessários para corrigir ou atualizar o seu cadastro.
+                              Encontre a DRE da sua Unidade Educacional no link:
+                              <br/>
+                              <a href="https://escolaaberta.sme.prefeitura.sp.gov.br/" target="blank">https://escolaaberta.sme.prefeitura.sp.gov.br/</a>
+                            </div>
+                          }
+
+                          {showStatus2 && 
+                            <div className="container-resultado cont-amarelo"> 
+                              Dirija-se a unidade escolar com os documentos necessários para corrigir ou atualizar o seu cadastro.
+                            </div>
+                          }
+
+                          {showStatus3 && 
+                            <div className="container-resultado cont-verde"> 
+                              Seu cadastro está completo, baixe o aplicativo e crie sua conta. 
+                              <br/>
+                              Mercado Pago:  <a href="https://youtu.be/pOTemAlTt6Q" target="blank">https://youtu.be/pOTemAlTt6Q</a>
+                              <br/>
+                              Blupay: <a href="https://blupay.com.br/materialescolar" target="blank">https://blupay.com.br/materialescolar</a>
+                            </div>
+                          }
+
+                          {showStatusNaoEncontrado && 
+                            <div className="container-resultado cont-vermelho"> 
+                              Procure sua unidade escolar.
+                            </div>
+                          }
+                        </div>
+                        
                     </div>
                 </div>
             </div>
-
-            <Login/>
 
         </Fragment>
     )
---
+++ src/pages/Home/index_atualizacao.js
@@ -0,0 +1,33 @@
+import React, {Fragment} from "react";
+import "./home.scss"
+import {Login} from '../../components/Formularios/Login'
+
+// Home antiga referente ao Atualizacao Cadastral. Renomeada quando o sistema se tornou o "Consulta de Cadastro"
+
+export const Home = () => {
+    return (
+        <Fragment>
+            <div className="w-100 banner-home position-relative">
+                <div className="container">
+                    <div className="conteudo">
+                        <div className="col-lg-7 col-sm-12 col-xl-5">
+                            <h1 id="conteudo">
+                                Atualize seus dados cadastrais.
+                            </h1>
+                        </div>
+                        <div className="col-lg-6 col-sm-12 col-xl-5">
+                            <p>
+                            Digite o Código EOL da(o) estudante e a data de nascimento dela(e), para abrir o formulário e completar os dados de cadastro da(o) responsável.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <Login/>
+
+        </Fragment>
+    )
+
+}
